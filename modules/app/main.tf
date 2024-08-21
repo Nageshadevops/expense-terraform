2dc76a07f011a74f608760a0a752203eac8c96a3
nagesha technology <asbconstruction19@gmail.com>
2024-08-21 14:15:57 +0530

    vault_setup_terraform_ansible

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -15,14 +15,16 @@ resource "null_resource" "ansible" {
 
     connection {
       type     = "ssh"
-      user     = var.ssh_user
-      password = var.ssh_pass
+      user     = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_user
+      password = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_password
       host     = aws_instance.instance.public_ip
     }
 
     inline = [
       "sudo pip3.11 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/Nageshadevops/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component}"
+      "ansible-pull -i localhost, -U https://github.com/Nageshadevops/expense-ansible get-secrets.yml -e env=${var.env} -e role_name=${var.component}  -e vault_token=${var.vault_token}",
+      "ansible-pull -i localhost, -U https://github.com/Nageshadevops/expense-ansible expense.yml -e env=${var.env} -e role_name=${var.component} -e @secrets.json -e @app.json",
+      "rm -f ~/secrets.json ~/app.json"
     ]
   }
 }
