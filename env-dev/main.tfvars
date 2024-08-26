3f022a37874b8f6c95de6c3911f1c7d60acdf075
nagesha technology <asbconstruction19@gmail.com>
2024-08-26 22:40:50 +0530

    module vpc to module app connection

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -4,8 +4,12 @@ zone_id           = "Z04907431HM3OFWU2B8S0"
 
 # VPC
 vpc_cidr_block            = "10.10.0.0/24"
-subnet_cidr_block         = "10.10.0.0/24"
 default_vpc_cidr          = "172.31.0.0/16"
 default_vpc_id            = "vpc-016fd7e642558e601"
 default_route_table_id    = "rtb-01019001dd28e0b3f"
 #vpc_peering_connection_id = "pcx-0b8cad40071d49a2a"
+
+frontend_subnets  = ["10.10.0.0/27", "10.10.0.32/27"]
+backend_subnets   = ["10.10.0.64/27", "10.10.0.96/27"]
+db_subnets        = ["10.10.0.128/27", "10.10.0.160/27"]
+availability_zones = ["us-east-1a", "us-east-1b"]
--- main.tf
+++ main.tf
@@ -1,40 +1,49 @@
-# module "frontend" {
-#   depends_on = [module.backend]
-#
-#   source        = "./modules/app"
-#   instance_type = var.instance_type
-#   component     = "frontend"
-#   env           = var.env
-#   zone_id       = var.zone_id
-#   vault_token   = var.vault_token
-# }
-#
-# module "backend" {
-#   depends_on = [module.mysql]
-#
-#   source        = "./modules/app"
-#   instance_type = var.instance_type
-#   component     = "backend"
-#   env           = var.env
-#   zone_id       = var.zone_id
-#   vault_token   = var.vault_token
-# }
-#
-# module "mysql" {
-#   source        = "./modules/app"
-#   instance_type = var.instance_type
-#   component     = "mysql"
-#   env           = var.env
-#   zone_id       = var.zone_id
-#   vault_token   = var.vault_token
-# }
+module "frontend" {
+  depends_on = [module.backend]
+
+  source        = "./modules/app"
+  instance_type = var.instance_type
+  component     = "frontend"
+  env           = var.env
+  zone_id       = var.zone_id
+  vault_token   = var.vault_token
+  subnets       = module.vpc.frontend_subnets
+  vpc_id        = module.vpc.vpc_id
+}
+
+module "backend" {
+  depends_on = [module.mysql]
+
+  source        = "./modules/app"
+  instance_type = var.instance_type
+  component     = "backend"
+  env           = var.env
+  zone_id       = var.zone_id
+  vault_token   = var.vault_token
+  subnets       = module.vpc.backend_subnets
+  vpc_id        = module.vpc.vpc_id
+}
+
+module "mysql" {
+  source        = "./modules/app"
+  instance_type = var.instance_type
+  component     = "mysql"
+  env           = var.env
+  zone_id       = var.zone_id
+  vault_token   = var.vault_token
+  subnets       = module.vpc.db_subnets
+  vpc_id        = module.vpc.vpc_id
+}
 
 module "vpc" {
   source                 = "./modules/vpc"
   env                    = var.env
   vpc_cidr_block         = var.vpc_cidr_block
-  subnet_cidr_block      = var.subnet_cidr_block
   default_vpc_id         = var.default_vpc_id
   default_vpc_cidr       = var.default_vpc_cidr
   default_route_table_id = var.default_route_table_id
+  frontend_subnets       = var.frontend_subnets
+  backend_subnets        = var.backend_subnets
+  db_subnets             = var.db_subnets
+  availability_zones     = var.availability_zones
 }
