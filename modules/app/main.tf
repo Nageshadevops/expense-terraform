3aa02ca3bd3338741b4bb6ea448c1fd3e972038f
nagesha technology <asbconstruction19@gmail.com>
2024-08-29 18:20:24 +0530

    Adding lb_target_group, lb_listener

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -93,26 +93,21 @@ resource "aws_lb_target_group" "main" {
   name     = "${var.env}-${var.component}-tg"
   port     = var.app_port
   protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-}
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  vpc_id   = var.vpc_id
 }
 
 resource "aws_lb_target_group_attachment" "main" {
-  count    = var.lb_needed ? 1 : 0
+  count            = var.lb_needed ? 1 : 0
   target_group_arn = aws_lb_target_group.main[0].arn
   target_id        = aws_instance.instance.id
   port             = var.app_port
 }
 
-
 resource "aws_lb_listener" "front_end" {
-  count    = var.lb_needed ? 1 : 0
+  count             = var.lb_needed ? 1 : 0
   load_balancer_arn = aws_lb.main[0].arn
   port              = var.app_port
-  protocol          = "HTTPS"
+  protocol          = "HTTP"
 
   default_action {
     type             = "forward"
